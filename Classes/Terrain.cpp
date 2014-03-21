8020577a134bb40bd5731a0a20da865375d0e0d8
Jeremias Nunez <jeremias.np@gmail.com>
2014-03-20 23:37:03 -0300

    added some helper files, organized folder structure

--- Classes/Terrain.cpp
+++ Classes/Terrain.cpp
@@ -8,14 +8,16 @@
 
 #include "Terrain.h"
 #include "MathUtils.h"
+#include "VisibleRect.h"
+
+USING_NS_CC;
+using namespace std;
 
 #define MAX_HILL_HORIZONTAL_DISTANCE 200
 #define MIN_HILL_HORIZONTAL_DISTANCE 40
 #define MIN_HILL_VERTICAL_DISTANCE 80
 #define HILL_TOP_OFFSET 200
-
-USING_NS_CC;
-using namespace std;
+#define HILL_SEGMENT_WIDTH 5
 
 #pragma mark - Lifecycle
 Terrain::Terrain()
@@ -35,12 +37,35 @@ Terrain::~Terrain()
 #pragma mark - Node Override
 void Terrain::draw()
 {
-    vector<Point> points = this->_hillKeyPoints;
-    Point prev = this->_hillKeyPoints.front();
+    Point p0 = this->_hillKeyPoints.front();
     
-    for (auto it = points.begin(); it != points.end(); ++it) {
-        DrawPrimitives::drawLine(prev, *it);
-        prev = *it;
+    for (Point p1 : this->_hillKeyPoints) {
+        glLineWidth(1);
+        DrawPrimitives::setDrawColor4B(255, 255, 255, 255);
+        DrawPrimitives::drawLine(p0, p1);
+        
+        int hSegments = floorf((p1.x - p0.x) / HILL_SEGMENT_WIDTH);
+        
+        float dx = (p1.x - p0.x) / hSegments;
+        float da = M_PI / hSegments;
+        float yMid = (p0.y + p1.y) / 2;
+        float ampl = (p0.y - p1.y) / 2;
+        
+        Point pt0, pt1;
+        pt0 = p0;
+        
+        for (int i = 0; i < hSegments + 1; ++i) {
+            pt1.x = p0.x + (i * dx);
+            pt1.y = yMid + ampl * cosf(da * i);
+            
+            glLineWidth(4);
+            DrawPrimitives::setDrawColor4B(255, 0, 0, 255);
+            DrawPrimitives::drawLine(pt0, pt1);
+            
+            pt0 = pt1;
+        }
+        
+        p0 = p1;
     }
 }
 
@@ -56,10 +81,8 @@ bool Terrain::onTouchBegan(Touch *touch)
 #pragma mark - Private
 void Terrain::generateHills()
 {
-    Size visibleSize = Director::getInstance()->getVisibleSize();
-    
     float currentX = 0;
-    float currentY = MathUtils::randIntInRange(MIN_HILL_VERTICAL_DISTANCE, visibleSize.height - HILL_TOP_OFFSET);
+    float currentY = MathUtils::randIntInRange(MIN_HILL_VERTICAL_DISTANCE, VisibleRect::height() - HILL_TOP_OFFSET);
     
     bool outOfScreen = false;
     
@@ -68,12 +91,12 @@ void Terrain::generateHills()
         this->_hillKeyPoints.push_back(hillPoint);
         
         currentX += MathUtils::randIntInRange(MIN_HILL_HORIZONTAL_DISTANCE, MAX_HILL_HORIZONTAL_DISTANCE);
-        currentY = MathUtils::randIntInRange(MIN_HILL_VERTICAL_DISTANCE, visibleSize.height - HILL_TOP_OFFSET);
+        currentY = MathUtils::randIntInRange(MIN_HILL_VERTICAL_DISTANCE, VisibleRect::height() - HILL_TOP_OFFSET);
         
-        if (currentX > visibleSize.width) {
+        if (currentX > VisibleRect::width()) {
             outOfScreen = true;
             
-            Point finalPoint = Point(visibleSize.width, currentY);
+            Point finalPoint = Point(VisibleRect::width(), currentY);
             this->_hillKeyPoints.push_back(finalPoint);
         }
     }
